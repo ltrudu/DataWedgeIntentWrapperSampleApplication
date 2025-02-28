35a0c0794f0a39d4a3da71521069c716063fb3e1
Trudu Laurent <laurent@trudu.com>
2025-02-28 10:40:13 +0100

    Added import profile test

--- DataWedgeIntentWrapperSampleApplication/src/main/java/com/symbol/datacapturereceiver/SecondActivity.java
+++ DataWedgeIntentWrapperSampleApplication/src/main/java/com/symbol/datacapturereceiver/SecondActivity.java
@@ -1,7 +1,9 @@
 package com.symbol.datacapturereceiver;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -25,8 +27,10 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Set;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class SecondActivity extends AppCompatActivity implements DWScanReceiver.onScannedData {
 
@@ -48,6 +52,9 @@ public class SecondActivity extends AppCompatActivity implements DWScanReceiver.
      */
     private Handler mScrollDownHandler = null;
     private Runnable mScrollDownRunnable = null;
+
+    private BroadcastReceiver mBroadcastReceiverImportCommand = null;
+    private static final String COMMAND_IDENTIFIER_IMPORT =" 1234560";
 
 
     @Override
@@ -78,12 +85,20 @@ public class SecondActivity extends AppCompatActivity implements DWScanReceiver.
             }
         });
 
-        Button btImport = (Button) findViewById(R.id.button_import);
-        btImport.setOnClickListener(new View.OnClickListener() {
+        Button btImportProfile = (Button)findViewById(R.id.button_import_profile);
+        btImportProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                importProfileFromDownloadFolder("dwprofile_com.symbol.datacapturereceiver");
+            }
+        });
+
+        Button btAutoImport = (Button) findViewById(R.id.button_auto_import);
+        btAutoImport.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //"database profile auto creation" import mode (filebased)
-                    importProfile("dwprofile_com.symbol.datacapturereceiver");
+                    importProfileFromAssets("dwprofile_com.symbol.datacapturereceiver");
             }
         });
 
@@ -194,6 +209,11 @@ public class SecondActivity extends AppCompatActivity implements DWScanReceiver.
             mScrollDownRunnable = null;
             mScrollDownHandler = null;
         }
+
+        if(mBroadcastReceiverImportCommand != null) {
+            unregisterReceiver(mBroadcastReceiverImportCommand);
+            mBroadcastReceiverImportCommand = null;
+        }
         super.onPause();
     }
 
@@ -231,7 +251,9 @@ public class SecondActivity extends AppCompatActivity implements DWScanReceiver.
             mScrollDownHandler.postDelayed(mScrollDownRunnable, 300);
     }
 
-    private void importProfile(String progileFilenameWithoutDbExtension) {
+
+
+    private void importProfileFromAssets(String progileFilenameWithoutDbExtension) {
             // Source : http://techdocs.zebra.com/datawedge/6-7/guide/settings/
             //Export your profile using
             //1. Open DataWedge
@@ -294,6 +316,175 @@ public class SecondActivity extends AppCompatActivity implements DWScanReceiver.
                         outputFile.renameTo(finalFile);
                 }
 
+            }
+        }
+
+    private void importProfileFromDownloadFolder(String progileFilenameWithoutDbExtension) {
+            // Source : http://techdocs.zebra.com/datawedge/6-7/guide/settings/
+            //Export your profile using
+            //1. Open DataWedge
+            //2. Open Hamburger Menu -> Settings (Paramètres)
+            //3. Open "Export" list entry
+            //4. Select profile to export
+            //5. Retrieve exported file in folder "\sdcard\Android\data\com.symbol.datawedge\files"
+
+            // Open the db as the input stream
+            InputStream fis = null;
+            FileOutputStream fos = null;
+            File outputFile = null;
+            File finalFile = null;
+
+            String targetDir = "/storage/emulated/0/Download/";
+            String temporaryFileName = progileFilenameWithoutDbExtension + ".tmp";
+            String finalFileName = progileFilenameWithoutDbExtension + ".db";
+
+            try {
+
+
+                fis = getAssets().open(finalFileName);
+
+
+                // create a File object for the parent directory
+                File outputDirectory = new File(targetDir);
+
+                // create a temporary File object for the output file
+                outputFile = new File(outputDirectory, temporaryFileName);
+                finalFile = new File(outputDirectory, finalFileName);
+
+                // attach the OutputStream to the file object
+                fos = new FileOutputStream(outputFile);
+
+                // transfer bytes from the input file to the output file
+                byte[] buffer = new byte[1024];
+                int length;
+                int tot = 0;
+                while ((length = fis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, length);
+                    tot += length;
+                }
+                Log.d(TAG, tot + " bytes copied");
+
+                //flush the buffers
+                fos.flush();
+
+                //release resources
+                fos.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                fos = null;
+                //set permission to the file to read, write and exec.
+                if (outputFile != null) {
+                    outputFile.setExecutable(true, false);
+                    outputFile.setReadable(true, false);
+                    outputFile.setWritable(true, false);
+                    //rename the file
+                    if (finalFile != null)
+                        outputFile.renameTo(finalFile);
+
+                    // Register a broadcast receiver
+                    mBroadcastReceiverImportCommand = new BroadcastReceiver() {
+                        @Override
+                        public void onReceive(Context context, Intent intent) {
+                            if (intent.getAction().equalsIgnoreCase(DataWedgeConstants.ACTION_RESULT_DATAWEDGE_FROM_6_2))
+                            {
+                                // L'intent reçu est bien l'intent cible
+                                // On trace le résultat
+                                String action = intent.getAction();
+                                String command = intent.getStringExtra("COMMAND");
+                                String commandidentifier = intent.getStringExtra("COMMAND_IDENTIFIER");
+                                String result = intent.getStringExtra("RESULT");
+
+                                if(commandidentifier == null)
+                                    return;
+
+                                if(commandidentifier.equalsIgnoreCase(COMMAND_IDENTIFIER_IMPORT) == false)
+                                    return;
+
+                                Bundle bundle = new Bundle();
+                                String resultInfo = "";
+
+                                if (intent.hasExtra("RESULT_LIST")) { // returns for COMPLETE_RESULT
+                                    ArrayList<Bundle> result_list = (ArrayList)intent.getSerializableExtra("RESULT_LIST");
+                                    for (Bundle bundleResult : result_list) {
+
+                                        resultInfo +="\n\n";
+
+                                        Set<String> keys = bundleResult.keySet();
+                                        for (String key : keys) {
+                                            String val = bundleResult.getString(key);
+                                            if (val == null) {
+
+                                                if (bundleResult.getStringArray(key) != null) {
+                                                    val = "";
+                                                    for (String s : bundleResult.getStringArray(key)) {
+                                                        val += "" + s + "\n";
+                                                    }
+                                                }
+                                            }
+
+                                            resultInfo += key + ": " + val + "\n";
+                                        }
+                                    }
+                                }
+                                else if (intent.hasExtra("RESULT_INFO")) {
+                                    bundle = intent.getBundleExtra("RESULT_INFO");
+                                    if(bundle != null) {
+                                        Set<String> keys = bundle.keySet();
+                                        for (String key : keys) {
+                                            String value = "";
+
+                                            if (bundle.getString(key) != null) {
+                                                value = bundle.getString(key);
+                                            } else if (bundle.getStringArray(key) != null) {
+                                                for (String innerString : bundle.getStringArray(key))
+                                                    value += innerString + ";";
+                                            }
+                                            if (resultInfo.length() > 0 && value != null)
+                                                resultInfo += "\n";
+                                            if (value != null)
+                                                resultInfo += key + ": " + value;
+                                        }
+                                    }
+                                }
+
+                                String text = "Action: " + action + "\n" +
+                                        "Command: " + command + "\n" +
+                                        "Result: " + result + "\n" +
+                                        "Result Info: " + resultInfo + "\n" +
+                                        "CID:" + commandidentifier;
+                                SecondActivity.this.addLineToResults(text);
+                                Log.d(TAG, text);
+                                SecondActivity.this.unregisterReceiver(mBroadcastReceiverImportCommand);
+                                mBroadcastReceiverImportCommand = null;
+                            }
+                        }
+                    };
+
+                    IntentFilter intentFilter = new IntentFilter();
+                    intentFilter.addAction(DataWedgeConstants.ACTION_RESULT_DATAWEDGE_FROM_6_2);
+                    intentFilter.addCategory(Intent.CATEGORY_DEFAULT);
+                    ContextCompat.registerReceiver(this, mBroadcastReceiverImportCommand, intentFilter, null, null, ContextCompat.RECEIVER_EXPORTED);
+
+                    // Create the Intent to import the file
+                    IntentFilter filter = new IntentFilter();
+                    filter.addAction(DataWedgeConstants.ACTION_RESULT_DATAWEDGE_FROM_6_2);
+                    filter.addCategory(Intent.CATEGORY_DEFAULT);
+
+                    Bundle bMain = new Bundle();
+                    bMain.putString("FOLDER_PATH", targetDir);
+                    ArrayList<String> fileNames = new ArrayList<>();
+                    fileNames.add(finalFileName);
+                    bMain.putStringArrayList("FILE_LIST", fileNames);
+
+                    Intent i = new Intent();
+                    i.setAction("com.symbol.datawedge.api.ACTION");
+                    i.putExtra("com.symbol.datawedge.api.IMPORT_CONFIG", bMain);
+                    i.putExtra("SEND_RESULT", "LAST_RESULT");
+                    i.putExtra("COMMAND_IDENTIFIER", COMMAND_IDENTIFIER_IMPORT);
+
+                    sendBroadcast(i);
+                }
             }
         }
 }
